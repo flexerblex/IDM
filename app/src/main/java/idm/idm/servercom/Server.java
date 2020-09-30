8b7edd2b6c7f56f00b57ba73e2eac08635fea717
flexerblex <flexerblex@gmail.com>
2020-09-30 16:14:12 -0400

    Login POST request and locking account

--- app/src/main/java/idm/idm/servercom/Server.java
+++ app/src/main/java/idm/idm/servercom/Server.java
@@ -10,11 +10,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
 
 /**
  * Used to make requests to a server.
@@ -24,29 +27,44 @@ public class Server {
 
     public static final Server SERVER = new Server();
 
-    private final String ADDRESS = "3.128.46.46";
+    private final String ADDRESS = "http://3.128.46.46/";
     private URL url;
 
-    public void login(String username, String password) {
-        Log.d("test", "Login Method Hit.");
+    private static String status;
+
+    public boolean login(String username, String password)
+    {
         try {
             new LoginRequest().execute(username, password).get();
+            if(status.contains("200"))
+                return true;
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
+        catch(Exception exc)
+        {
+            System.out.println(exc.getMessage());
         }
+        return false;
     }
 
+    private class LoginRequest extends AsyncTask<String, Integer, JSONObject>
+    {
 
-    private class LoginRequest extends AsyncTask<String, Integer, JSONObject> {
-
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+        }
         @Override
         protected JSONObject doInBackground(String... strings) {
             try {
                 loginTask(strings[0], strings[1]);
             }
-            catch(JSONException e) {
-                System.out.println(e.getMessage());
+            catch(JSONException jsonexc)
+            {
+                System.out.println(jsonexc.getMessage());
                 System.exit(1);
             }
             return null;
@@ -56,26 +74,48 @@ public class Server {
     private void loginTask (String username, String password) throws JSONException {
 
         try {
-            url = new URL("http://3.128.46.46/");
 
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            Log.d(conn.toString(), "HttpURLConnection established...");
+            url = new URL(ADDRESS+"login");
 
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-Type", "application/json; utf-8");
-            conn.setRequestProperty("Accept", "application/json");
+            JSONObject loginData = new JSONObject();
+            loginData.put("username", username);
+            loginData.put("password", password);
 
-            try(BufferedReader br = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream(), "utf-8"))) {
-                StringBuilder response = new StringBuilder();
-                String responseLine = null;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
-                System.out.println(response.toString());
-                Log.d(response.toString(), "SERVER RESPONSE");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            Log.d(con.toString(), "HttpURLConnection established...");
+
+            con.setRequestMethod("POST");
+            //con.setDoOutput(true); //don't include this in POST request
+            con.setRequestProperty("Content-Type", "application/json");
+
+            DataOutputStream outputStream = new DataOutputStream(con.getOutputStream());
+            outputStream.writeBytes(loginData.toString());
+            outputStream.flush();
+            outputStream.close();
+
+            Log.d(loginData.toString(), "LOGIN DATA");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            System.out.println("Got inputStream: " + in);
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
+            in.close();
+
+            String verification = con.getHeaderField("status");
+            System.out.println("Header Field: " + verification);
+
+            System.out.println("Response: " + response.toString());
+
+            status = response.toString();
+
+            con.disconnect();
+
         }
+
         catch(IOException exc)
         {
             System.out.println(exc.getMessage());
