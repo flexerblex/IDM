e04960bee39316d83d207e38e4c951f15268b0cb
Daniel Bilan <danbilan@gmail.com>
2020-10-28 23:51:12 -0400

    add logout button to activity_home.xml (this is a test commit)

---
+++ app/src/main/java/idm/idm/Logout.java
@@ -0,0 +1,32 @@
+package idm.idm;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Logout {
+
+    private Button Logout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+
+
+        Logout = (Button)findViewById(R.id.logoutButton);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
