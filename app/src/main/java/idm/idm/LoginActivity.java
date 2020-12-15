0f6c6284c1d6c5e8516c1af148b9824dd1f9d8f8
chengpan233 <zoepan95@gmail.com>
2020-12-15 14:27:38 +0800

    Merge branch 'Lexy' of C:\Users\Guoli zhang\StudioProjects\IDM with conflicts.

--- app/src/main/java/idm/idm/LoginActivity.java
+++ app/src/main/java/idm/idm/LoginActivity.java
@@ -3,7 +3,9 @@ package idm.idm;
 import android.content.Context;
 import android.content.Intent;
 //import android.icu.text.SimpleDateFormat;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -14,6 +16,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,18 +26,23 @@ import java.io.InputStream;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import org.json.JSONException;
 
+import org.json.JSONObject;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
+//import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
+//import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
@@ -43,25 +51,31 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.Executor;
 
 import idm.idm.servercom.FaceRecognizer;
 import idm.idm.servercom.Server;
 
-public class LoginActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2
- {
+public class LoginActivity extends AppCompatActivity  {
 
     private EditText Name;
     private EditText Password;
     private TextView Info;
     private Button Login;
-    private ImageButton LoginFace;
+    private Button LoginFace;
+    private Button LoginFingerprint;
+    private Button LoginVoice;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
     private TextView Create;
     private int counter = 3;
     private Date lockTime;
-    JavaCameraView javaCameraView;
-    File cascFile;
-    CascadeClassifier faceDetector;
-    private Mat mRgba, mGrey;
+    //JavaCameraView javaCameraView;
+    //File cascFile;
+    //CascadeClassifier faceDetector;
+    //private Mat mRgba, mGrey;
 
     private String currentPhotoPath;
     private File imageFile;
@@ -77,10 +91,12 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
         Info = (TextView)findViewById(R.id.logInfo);
 
         Login = (Button)findViewById(R.id.loginButton);
-        LoginFace = (ImageButton)findViewById((R.id.faceID));
+        LoginFace = (Button)findViewById((R.id.faceID));
+        LoginFingerprint = (Button)findViewById(R.id.fingerprintID);
+        LoginVoice = (Button)findViewById(R.id.voiceID);
         Create = (TextView) findViewById(R.id.createAccount);
 
-        javaCameraView = (JavaCameraView)findViewById(R.id.javaCamView);
+        //javaCameraView = (JavaCameraView)findViewById(R.id.javaCamView);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,11 +120,64 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
                             "idm.idm.provider", imageFile);
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    intent.putExtra("android.intent.extras.CAMERA_FACING", android.hardware.Camera.CameraInfo.CAMERA_FACING_FRONT);
+                    intent.putExtra("android.intent.extras.LENS_FACING_FRONT", 1);
+                    intent.putExtra("android.intent.extra.USE_FRONT_CAMERA", true);
                     startActivityForResult(intent,1);
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        });
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                SharedPreferences prefs = getSharedPreferences("UserData", MODE_PRIVATE);
+                String username = prefs.getString("username","");
+                String pwd = prefs.getString("password","");
+                if (username.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Error: User has not set up Fingerprint yet", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    validID(username, pwd);
+                }
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login using your fingerprint")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        LoginFingerprint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                biometricPrompt.authenticate(promptInfo);
             }
         });
 
@@ -120,19 +189,14 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
             }
         });
 
-        //Camera view
-        if(!OpenCVLoader.initDebug()){
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION,this,baseCallback);
-        }
-        else{
-            try {
-                baseCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
-        javaCameraView.setCvCameraViewListener(this);
+        LoginVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRecord = new Intent(LoginActivity.this,RecordAudioActivity.class);
+                startActivity(toRecord);
+            }
+        });
 
     }
 
@@ -147,12 +211,9 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
             counter--;
             Info.setText("ATTEMPTS REMAINING: " + String.valueOf(counter));
 
-            //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
             if(counter == 0){
-                lockTime = new Date();
-                //Login.setEnabled(false);
-                Info.setText("Try 2 minutes later.");
+                Server.SERVER.Lock(Name.getText().toString(), "lock");
+                Info.setText("You've been locked out.");
             }
             else if (counter < 0) {
 
@@ -166,31 +227,34 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
                         Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
-                    }else{
+                    }
+                    else{
                         counter --;
                         Info.setText("ATTEMPTS REMAINING: " + String.valueOf(counter));
                     }
                 }
                 else {
                     //Login.setEnabled(false);
-                    Info.setText("Try " + String.valueOf(minutes) + " minutes later.");
+                    Info.setText("You've been locked out");
                 }
             }
+
         }
-    }
+   }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        String username = "Dummy";
+        String username = Name.getText().toString();
 
         if (requestCode == 1 && resultCode == RESULT_OK ) {
 
             if (FaceRecognizer.FACERECOGNIZER.Authenticate(imageFile, username)) {
                 Intent toHome2 = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(toHome2);
+
             }
             else {
                 System.out.println("Error authenticating.");
@@ -198,78 +262,76 @@ public class LoginActivity extends AppCompatActivity implements CameraBridgeView
         }
     }
 
-  
       //Camera
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat();
-        mGrey = new Mat();
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-        mGrey.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        mGrey = inputFrame.gray();
-
-        //detect face
-        MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(mRgba,faceDetections);
-
-
-        for(Rect rect: faceDetections.toArray()){
-            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
-                    new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(255,0,0));
-        }
-        return mRgba;
-    }
-
-    private BaseLoaderCallback baseCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) throws IOException {
-            switch (status){
-                case LoaderCallbackInterface.SUCCESS:{
-                    InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
-                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                    cascFile = new File(cascadeDir,"haarcascade_frontalface_alt2.xml");
-
-                    FileOutputStream fos = new FileOutputStream(cascFile);
-
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
-
-                    while((bytesRead = is.read(buffer))!=-1){
-                        fos.write(buffer,0,bytesRead);
-                    }
-
-                    is.close();
-                    fos.close();
-
-                    faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
-
-                    if(faceDetector.empty()){
-                        faceDetector = null;
-                    }
-                    else{
-                        cascadeDir.delete();
-                    }
-                    javaCameraView.enableView();
-                }
-                break;
-
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
-
-        }
-    };
-
+//    @Override
+//    public void onCameraViewStarted(int width, int height) {
+//        mRgba = new Mat();
+//        mGrey = new Mat();
+//    }
+//
+//    @Override
+//    public void onCameraViewStopped() {
+//        mRgba.release();
+//        mGrey.release();
+//    }
+//
+//    @Override
+//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//        mRgba = inputFrame.rgba();
+//        mGrey = inputFrame.gray();
+//
+//        //detect face
+//        MatOfRect faceDetections = new MatOfRect();
+//        faceDetector.detectMultiScale(mRgba,faceDetections);
+//
+//
+//        for(Rect rect: faceDetections.toArray()){
+//            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
+//                    new Point(rect.x + rect.width, rect.y + rect.height),
+//                    new Scalar(255,0,0));
+//        }
+//        return mRgba;
+//    }
+//
+//    private BaseLoaderCallback baseCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) throws IOException {
+//            switch (status){
+//                case LoaderCallbackInterface.SUCCESS:{
+//                    InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+//                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+//                    cascFile = new File(cascadeDir,"haarcascade_frontalface_alt2.xml");
+//
+//                    FileOutputStream fos = new FileOutputStream(cascFile);
+//
+//                    byte[] buffer = new byte[4096];
+//                    int bytesRead;
+//
+//                    while((bytesRead = is.read(buffer))!=-1){
+//                        fos.write(buffer,0,bytesRead);
+//                    }
+//
+//                    is.close();
+//                    fos.close();
+//
+//                    faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+//
+//                    if(faceDetector.empty()){
+//                        faceDetector = null;
+//                    }
+//                    else{
+//                        cascadeDir.delete();
+//                    }
+//                    javaCameraView.enableView();
+//                }
+//                break;
+//
+//                default: {
+//                    super.onManagerConnected(status);
+//                }
+//                break;
+//            }
+//
+//        }
+//    };
 }
